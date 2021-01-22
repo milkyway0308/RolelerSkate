a690f1f5d482fdffeae59a192dc597e8204e3a41
Wat da fak <juni2484ok@naver.com>
2021-01-23 01:43:43 +0900

    RolelerSkate Alpha 0.0.16 - 이 코드는 무료로 당신의 눈을 파괴해드립니다! - 하하 레거시 코드나 맛봐라 이 개발자 놈들아 - 더 많은 레거시나 맛봐라 하하!

--- src/main/java/skywolf/rolelerskate/RolelerSkate.java
+++ src/main/java/skywolf/rolelerskate/RolelerSkate.java
@@ -11,6 +11,7 @@ import discord4j.core.object.reaction.ReactionEmoji;
 import discord4j.core.shard.GatewayBootstrap;
 import discord4j.gateway.intent.IntentSet;
 import discord4j.rest.util.Color;
+import skywolf.rolelerskate.apple.AppleProductFinder;
 import skywolf.rolelerskate.eve.universe.JumpCatcher;
 import skywolf.rolelerskate.eve.universe.KillMailCatcher;
 import skywolf.rolelerskate.eve.universe.SystemSearcher;
@@ -28,7 +29,7 @@ import java.util.stream.Collectors;
 @SuppressWarnings("ALL")
 public class RolelerSkate {
 
-    private static final String VERSION = "Alpha 0.0.15";
+    private static final String VERSION = "Alpha 0.0.18";
 
     private static ExecutorService EXECUTOR = Executors.newCachedThreadPool();
 
@@ -53,7 +54,7 @@ public class RolelerSkate {
     }
 
     public static void main(final String[] args) {
-        final String token = "Your discord bot token here";
+        final String token = "Your bot token here";
 //        System.out.println(token);
         final DiscordClient client = DiscordClient.create(token);
 
@@ -516,6 +517,72 @@ public class RolelerSkate {
                         }).block();
                     }
                 });
+            } else if (message.getContent().startsWith("Apple.searchAccessories(")) {
+                if (!message.getContent().endsWith(");"))
+                    return;
+                String substring = message.getContent();
+                substring = substring.substring(substring.indexOf("(") + 1, substring.lastIndexOf(")")).trim();
+                if (substring.isEmpty()) {
+                    message.getChannel().block().createEmbed(spec -> {
+                        spec.setColor(Color.RED)
+                                .addField("오류!", "반드시 검색어를 입력해야 합니다.", false);
+                    }).block();
+                    return;
+                }
+                Message msg = message.getChannel().block().createEmbed(spec -> {
+                    spec.setColor(Color.GRAY)
+                            .addField("검색중..", "애플 스토어에서 데이터를 받아오는 중입니다.", false)
+                            .setFooter("RolelerSkate " + VERSION, null);
+                }).block();
+                String finalSubstring = substring;
+                EXECUTOR.submit(() -> {
+                    try {
+                        List<AppleProductFinder.AppleProduct> product = AppleProductFinder.find(finalSubstring);
+                        if (product == null) {
+                            msg.edit(mSpec -> {
+                                mSpec.setEmbed(spec -> {
+                                    spec.setColor(Color.RED)
+
+                                            .addField("오류", "애플 스토어에서 데이터를 받아올 수 없습니다.", false)
+                                            .setFooter("RolelerSkate " + VERSION, null);
+                                });
+                            }).block();
+                            return;
+                        }
+                        if(product.size() <= 0){
+                            msg.edit(mSpec -> {
+                                mSpec.setEmbed(spec -> {
+                                    spec.setColor(Color.RED)
+                                            .addField("오류", "애플 스토어에서 일치하는 데이터를 찾지 못했습니다.", false)
+                                            .setFooter("RolelerSkate " + VERSION, null);
+                                });
+                            }).block();
+                            return;
+                        }
+                        AppleProductFinder.AppleProduct pr = product.get(0);
+                        msg.edit(mSpec -> {
+                            mSpec.setEmbed(spec -> {
+                                spec.setColor(Color.GREEN)
+                                        .setTitle(pr.getName())
+                                        .addField("가격", pr.getPrice(), true)
+                                        .addField("신상 여부", String.valueOf(pr.isNew()), true)
+                                        .setUrl(pr.getProductURL())
+                                        .setImage(pr.getImageURL())
+                                        .setFooter("RolelerSkate " + VERSION, null);
+                            });
+                        }).block();
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        message.getChannel().block().createEmbed(spec -> {
+                            spec.setColor(Color.RED)
+                                    .addField(getRandomMessage(), "애석하게도 새로운 오류가 발생한 것 같군요.", false)
+                                    .addField(ex.getClass().getName(), ex.getMessage(), false)
+                                    .setFooter("RolelerSkate " + VERSION + " | Error", null);
+                        }).block();
+                    }
+                });
+
             }
         });
 
---
+++ src/main/java/skywolf/rolelerskate/apple/AppleProductFinder.java
@@ -0,0 +1,91 @@
+package skywolf.rolelerskate.apple;
+
+import discord4j.core.util.EntityUtil;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleProductFinder {
+
+    public static List<AppleProduct> find(String text) throws IOException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet get = new HttpGet("https://www.apple.com/us/search/" + URLEncoder.encode(text, "utf-8") + "?sel=accessories&src=serp");
+        String x = EntityUtils.toString(httpClient.execute(get).getEntity());
+        Document docs = Jsoup.parse(x);
+        Element el = docs.selectFirst("div.as-accessories-header > div > h1");
+        if (el == null)
+            return null;
+        String xg = el.text();
+        int index = Integer.parseInt(xg.substring(text.length(), xg.indexOf(" ", text.length())));
+        List<AppleProduct> products = new ArrayList<>();
+        if (index <= 0)
+            return products;
+        Elements ell = docs.select("#as-accessories > div.as-accessories-results.as-search-desktop > div.column.small-12.as-search-results-tiles.as-search-results-width > div");
+        for (Element e : ell) {
+            AppleProduct prdt = new AppleProduct();
+//            prdt.url = e.selectFirst("img").attr("src");
+            prdt.productURL = "https://www.apple.com/" + e.selectFirst("a").attr("href");
+            String texter = e.selectFirst("a").attr("href");
+            texter = texter.substring(texter.indexOf("product/") + "product/".length());
+            texter = texter.substring(0, texter.indexOf("/") - 2);
+            prdt.url = "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/" + texter + "?wid=445&hei=445&fmt=jpeg";
+            prdt.name = e.select("a > span:nth-child(1)").text();
+            prdt.isNew = !e.select("a > span:nth-child(2)").text().trim().isEmpty();
+
+            prdt.price = e.select("div.as-price-currentprice.as-producttile-currentprice").text();
+            products.add(prdt);
+//            System.out.println("X1...");
+        }
+//        System.out.println("X2...");
+        return products;
+    }
+
+    public static class AppleProduct {
+        private String name;
+        private String url;
+        private String productURL;
+        private String price;
+        private List<String> colors = new ArrayList<>();
+        private boolean isNew = false;
+        // 애플 독점?
+        private boolean onlyAtApple = false;
+
+        public String getImageURL() {
+            return url;
+        }
+
+        public String getProductURL() {
+            return productURL;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public List<String> getColors() {
+            return colors;
+        }
+
+        public boolean isNew() {
+            return isNew;
+        }
+
+        public boolean isOnlyAtApple() {
+            return onlyAtApple;
+        }
+    }
+}
